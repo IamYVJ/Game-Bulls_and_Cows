087cc87b76a5af5a1597c7900c46f6169bc3b303
Yashvardhan Jain <yashvardhan18@yahoo.com>
2021-03-04 02:57:32 +0530

    Minor Fix

--- compgame.py
+++ compgame.py
@@ -45,7 +45,8 @@ def get_guess():
     guess = input('Enter Guess: ').upper().strip()
     if guess=='IQUIT':
         return guess
-    while len(guess)!=4 and guess not in dictionary[word[0]]:
+    while len(guess)!=4 or guess not in dictionary[guess[0]]:
+        print('Invalid Guess. Try Again!')
         guess = input('Enter Guess: ').upper().strip()
         if guess=='IQUIT':
             break
@@ -59,20 +60,37 @@ def get_dictionary():
     with open(dictionary_path) as f:
         dictionary = json.loads(f.read())
 
+def print_line():
+    print()
+    print('-----------------------------------------------------------------------------')
+    print()
+
+def print_header():
+    print()
+    print_line()
+    print('\t\t\tBulls and Cows - By YVJ Systems')
+    print('\t\t\t\tVersus Computer')
+    print_line()
 
 def main():
+    print_header()
     get_dictionary()
     word = get_random_word()
     game_over = False
+    attempts = 0
     while not game_over:
         guess = get_guess()
         if guess=='IQUIT':
             print(f'Word: {word}')
+            print(f'You quit after {attempts} attempts!')
             game_over=True
-        bulls, cows = check(word, guess)
-        print(f'{bulls} Bulls & {cows} Cows\n')
-        if bulls==4:
-            game_over=True
+        else:
+            attempts +=1
+            bulls, cows = check(word, guess)
+            print(f'{bulls} Bulls & {cows} Cows\n')
+            if bulls==4:
+                print(f'You win in {attempts} attempts!')
+                game_over=True
 
 if __name__ == "__main__":
     try:
